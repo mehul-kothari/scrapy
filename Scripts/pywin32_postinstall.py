daac4e9e8c5b35dfd72dae50baa4cc6455459f8f
mehul-kothari <mehulkothari1996@gmail.com>
2018-08-16 23:06:03 +0530

    scrapper

---
+++ Scripts/pywin32_postinstall.py
@@ -0,0 +1,4 @@
+#!c:\scrapy_1\venv1\scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'pywin32==220',u'pywin32_postinstall.py'
+__requires__ = 'pywin32==220'
+__import__('pkg_resources').run_script('pywin32==220', u'pywin32_postinstall.py')
